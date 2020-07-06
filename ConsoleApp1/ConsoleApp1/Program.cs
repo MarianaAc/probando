b6657733ed1deaf8d820c47b432d949a28ebba6c
Mariana Acosta <marianaacosta193@gmail.com>
2020-07-05 21:43:11 -0300

    nxt

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Por fin");
+            Console.WriteLine("Vamos");
         }
     }
 }
